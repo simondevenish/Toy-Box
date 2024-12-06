f4eb610ffc4929c9951e30b85a4b025c7192d581
Simon Jordan <simon@thepolygonloft.com>
2024-12-06 20:48:37 +0000

    Change to SDL2 over SDL3

--- main.cpp
+++ main.cpp
@@ -1,45 +1,56 @@
 #include <SDL3/SDL.h>
 #include <stdio.h>
 
+// Constants
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
-#define WINDOW_TITLE "Toy Box"
 
+// Function prototypes
 void HandleEvents(int* isRunning);
 
 int main(int argc, char* argv[]) {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
-        fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
-        return -1;
+    SDL_Window* window; // Declare a pointer
+    int isRunning = 1;  // Application running flag
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        const char* sdlError = SDL_GetError();
+        if (sdlError && sdlError[0] != '\0') {
+            SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL: %s\n", sdlError);
+        } else {
+            SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL: Unknown error\n");
+        }
+        return 1;
     }
 
-    // Create an SDL window
-    SDL_Window* window = SDL_CreateWindow(
-        WINDOW_TITLE,                      // Window title
-        WINDOW_WIDTH,                      // Window width
-        WINDOW_HEIGHT,                     // Window height
-        SDL_WINDOW_VULKAN                  // Vulkan flag for future Kajiya integration
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "Toy Box",                  // Window title
+        WINDOW_WIDTH,               // Width, in pixels
+        WINDOW_HEIGHT,              // Height, in pixels
+        SDL_WINDOW_VULKAN           // Flags - see below
     );
 
-    if (!window) {
-        fprintf(stderr, "Failed to create SDL window: %s\n", SDL_GetError());
+    // Check that the window was successfully created
+    if (window == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         SDL_Quit();
-        return -1;
+        return 1;
     }
 
-    // SDL event loop
-    int isRunning = 1;
+    // Main event loop
     while (isRunning) {
         HandleEvents(&isRunning);
 
-        // Placeholder for future kajiya rendering logic
+        // Placeholder for rendering logic (if any)
+        SDL_Delay(16); // Limit to ~60 FPS
     }
 
-    // Clean up
+    // Close and destroy the window
     SDL_DestroyWindow(window);
-    SDL_Quit();
 
+    // Clean up
+    SDL_Quit();
     return 0;
 }
 
@@ -47,11 +58,11 @@ void HandleEvents(int* isRunning) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
-            case SDL_EVENT_QUIT:
+            case SDL_EVENT_QUIT: // Renamed from SDL_QUIT
                 *isRunning = 0;
                 break;
-            case SDL_EVENT_KEY_DOWN:
-                if (event.key.scancode == SDL_SCANCODE_ESCAPE) {
+            case SDL_EVENT_KEY_DOWN: // Renamed from SDL_KEYDOWN
+                if (event.key.scancode == SDL_SCANCODE_ESCAPE) { // Direct access to scancode
                     *isRunning = 0;
                 }
                 break;
@@ -60,4 +71,3 @@ void HandleEvents(int* isRunning) {
         }
     }
 }
-
